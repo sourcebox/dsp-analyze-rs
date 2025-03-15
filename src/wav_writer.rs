1b5acab6d4e5ed9a29495a8c27408164d05a401b
Oliver Rockstedt <info@sourcebox.de>
2025-03-15 13:26:32 +0100

    Replaced deprecated wav dependency by hound

--- src/wav_writer.rs
+++ src/wav_writer.rs
@@ -1,20 +1,34 @@
 //! Writer for WAV files
 
-use std::fs::File;
 use std::path::Path;
+
+use hound::*;
 
 /// Writes sample data as WAV file in 32-bit float format.
 pub fn write(
     filename: impl AsRef<std::path::Path> + core::fmt::Display,
     sample_rate: u32,
-    data: &[f32],
+    samples: &[f32],
 ) -> std::io::Result<()> {
     let path = format!("{filename}");
     let path = Path::new(path.as_str());
+
+    // Create parent directories to the path if they don't exist.
     let parent = path.parent().unwrap();
     std::fs::create_dir_all(parent).ok();
-    let mut file = File::create(path)?;
-    let header = wav::Header::new(wav::WAV_FORMAT_IEEE_FLOAT, 1, sample_rate, 32);
-    wav::write(header, &wav::BitDepth::from(Vec::from(data)), &mut file)?;
+
+    let spec = WavSpec {
+        channels: 2,
+        sample_rate: sample_rate,
+        bits_per_sample: 32,
+        sample_format: SampleFormat::Float,
+    };
+    let mut writer = WavWriter::create(path, spec).unwrap();
+
+    for sample in samples {
+        writer.write_sample(*sample).unwrap();
+        writer.write_sample(*sample).unwrap();
+    }
+
     Ok(())
 }
